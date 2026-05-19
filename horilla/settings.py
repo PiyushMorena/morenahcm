b8f03afae2cf1860531672732ca4a4ecc88810d1
PiyushMorena <piyush@morenatechnologies.com>
2026-05-19 18:42:50 +0530

    Update settings.py

--- horilla/settings.py
+++ horilla/settings.py
@@ -43,8 +43,16 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+
 ALLOWED_HOSTS = [
     ".onrender.com",
+    "morenahcm.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://morenahcm.onrender.com",
 ]
 
 # Application definition
